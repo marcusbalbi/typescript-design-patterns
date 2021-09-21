172a335f00b0b38a63199031adbfe2f09821a116
Marcus Balbi <balbimarcus@gmail.com>
2021-09-20 22:14:23 -0300

    explaining decorator

--- src/decorator/cloth-store/ClothingPriceDecorator.ts
+++ src/decorator/cloth-store/ClothingPriceDecorator.ts
@@ -2,12 +2,15 @@ import { Clothing } from "./Clothing";
 
 export abstract class ClothingPriceDecorator extends Clothing {
   protected clothing: Clothing;
+
   constructor(clothing: Clothing) {
+
     super({
       name: clothing.getName(),
       price: clothing.getPrice(),
       type: clothing.getType(),
     });
+
     this.clothing = clothing;
   }
 }
